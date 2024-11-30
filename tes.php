354d4e566fcfc1bce854a7eaa1ab66061d430dcc
AlyaNurfadilah <alyanurfadilah882@gmail.com>
2024-11-30 09:24:30 +0700

    Add files via upload

---
+++ tes.php
@@ -0,0 +1,24 @@
+<?php
+function nama(){
+    echo"hallo alya nurfadilah";
+}
+
+nama();
+
+echo"<br>";
+
+function pengurangan($nilai3,$nilai1){
+    $hasil = $nilai3 - $nilai1;
+    return $hasil;
+}
+echo(pengurangan(67,65));
+
+    $jumlah = $angka3 * $angka1;
+    return $jumlah;
+echo "<br>";
+
+function perkalian ($angka3,$angka1){
+    $jumlah = $angka3 * $angka1;
+    return $jumlah;
+}
+
